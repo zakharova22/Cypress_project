07db68187a004c4100419d6f6022940a5660d892
zakharova22 <zakharovav22@gmail.com>
2023-03-27 16:09:48 -0700

    delete all code for master

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,13 +1,46 @@
-
-//describe ('Mobile phone replenishment', () => {
+describe ('Mobile phone replenishment', () => {
     
     //context("Replenishment less than the allowed amount", ()=> {
       //it("Show error: Minimum amount of the replenishment 1 UAH", () =>{});
     //});
   
-//});
+});
 
-//Comment
+it('By ID', () => {
+  cy.visit("https://www.facebook.com/")
+  cy.get('#email')
+});
+
+it('By Class', () => {
+  cy.visit("https://docs.cypress.io/guides/overview/why-cypress")
+  cy.get('.ds-input')
+});
+
+it('By Tag', () => {
+  cy.visit("https://docs.cypress.io/guides/overview/why-cypress")
+  cy.get('navbar')
+});
+
+it.only('By Tag value', () => {
+  cy.visit("https://www.facebook.com/")
+  cy.get('[name="pass"]')
+});
+
+it.only('By Diffent Tag', () => {
+  cy.visit("https://www.facebook.com/")
+  cy.get('[data-testid="open-registration-form-button"][role="button"]')
+});
+
+it.only('By Diffent Tag', () => {
+  cy.visit("https://docs.cypress.io/guides/overview/why-cypress")
+  cy.get('button[type="button"][title ="close banner"]')
+});
+
+it.only('By Contains name', () => {
+  cy.visit("https://next.privat24.ua/")
+  cy.get('*[class^="card"]')
+});
+
 
 it('Using Get with Find and Eq', () => {
   cy.visit('https://next.privat24.ua/deposit/open')
